744dd481a7d9b1a424058464c2263eb081a32540
Quan-Ng <QNguyen7#07@gmail.com>
2024-06-27 07:59:32 -0700

    LAB4.5.7:DONE

--- main.py
+++ main.py
@@ -5,7 +5,20 @@ def main():
     Code Your Program here
     ########################################
     """
-
+    
+    prev_val = None
+    
+    while True:
+        cur_val = int(input("Enter Value Here: "))
+        
+        if prev_val is None or cur_val <= prev_val:
+            numbers.append(cur_val)
+            prev_val = cur_val
+        else:
+            break
+        
+        
+    
     ########################################
     # Do not delete the return statement
     ########################################
